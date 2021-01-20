0f61b4a2d2d632016a95bd13ab2dd95c52e57ed6
Danish Ferraltri <31229773+lightningflashcode@users.noreply.github.com>
2021-01-20 14:27:36 +0700

    Add files via upload
    uploaded vile from lenovo project mailchimp with data  node modules

---
+++ mailist.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const https = require("https");
+const client = require("@mailchimp/mailchimp_marketing");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+client.setConfig({
+  apiKey: 'dd0e5c78f492a7c5e94131e43a353395-us7',
+  server: 'us7',
+});
+
+app.post("/", function(req, res){
+  const firstName = req.body.first;
+  const lastName = req.body.last;
+  const email = req.body.email;
+  console.log('first name :',  firstName,',  last name :', lastName,', email :',  email);
+
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email
+  }
+
+    const run = async () => {
+     try{    
+        const response = await client.lists.addListMember('744a7a34a0', {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+            FNAME: subscribingUser.firstName,
+            LNAME: subscribingUser.lastName
+        }
+      });
+           console.log(response);
+      res.sendFile(__dirname + "/success.html");
+    } catch (err) {
+      console.log(err.status);
+      res.sendFile(__dirname + "/failure.html");
+    }
+  };
+ 
+  run();
+});
+ 
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.");
+});
